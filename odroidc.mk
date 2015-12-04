d770fb482e4881ba18eea597d7c4b8b70106341b
codewalker <codewalker@hardkernel.com>
2015-12-04 12:57:49 +0900

    ODROID-C1: Add USBIO Sensor Board.
    Change-Id: I1d0be2816e5515ae74e0831f5251b71ab5963d73

--- odroidc.mk
+++ odroidc.mk
@@ -73,6 +73,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     alsa.mixer.capture.earpiece=Digital
 endif
 
+# ODROID USBIO-SENSOR
+BOARD_HAVE_ODROID_SENSOR := true
+
 #########################################################################
 #
 #                                                USB
@@ -217,7 +220,8 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
 	frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
 	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
-	frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
+	frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
+	frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml
 
 
 PRODUCT_COPY_FILES += \
