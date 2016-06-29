fad0dace139941d9521aaf1e51e759fc874ac616
Luke Go <luke.go@hardkernel.com>
2016-06-29 14:15:53 +0900

    ODROID-C1: Removed the unnecessary while loop.
    - It avoid to the unnecessary copy from boot.ini.template
    
    Change-Id: Id2a6d7d55df58b92f028b74bf7c91f2063763f48

--- makebootini.sh
+++ makebootini.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
-
-while true
-do
-    sleep 1
-    if [ -f "/storage/sdcard0/boot.ini" ]
-    then
-        break
-    else
-        cp /system/etc/boot.ini.template /storage/sdcard0/boot.ini
-    fi
-done
+if [ -f "/storage/sdcard0/boot.ini" ]
+then
+    break
+else
+    cp /system/etc/boot.ini.template /storage/sdcard0/boot.ini
+fi
